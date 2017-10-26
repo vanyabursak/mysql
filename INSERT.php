db9589f9f568e907d7bbfea66d43935b3175b671
ivan <bursakivan@gmail.com>
2017-10-26 14:21:33 +0300

    INSERT

--- INSERT.php
+++ INSERT.php
@@ -4,15 +4,40 @@ $username = "root";
 $password = "";
 $dbname = "test";
 
+
+//обектно ориентированный способ
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 $sql = "INSERT INTO users(name, surname,password)
-        VALUES ('koka', 'kogut','3425'),
-              ('kokaaaaa', 'kogut','3425544')";
+        VALUES ('koka', 'kogut','3425');";
+//$sql.= "INSERT INTO users(name, surname,password)
+//        VALUES ('koka1', 'kogut1','3425111');";
+//$sql.= "INSERT INTO users(name, surname,password)
+//        VALUES ('koka2', 'kogut2','3425222')";
+//
+//
+//if($conn->multi_query($sql)){// чтобы записывать в переменную $sql много раз фун multi_query
+//    echo "record created";
+//}
+//
+//$conn->close();
 
 
-if($conn->query($sql)){
-    echo "record created";
-}
 
-$conn->close();
+//PDO
+//try {
+//    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username,$password);
+//    $conn->beginTransaction();
+//    $conn->exec("INSERT INTO users(name,surname,password)
+//      VALUES ('Koka','Kogut','1234')");
+//    $conn->exec("INSERT INTO users(name,surname,password)
+//      VALUES ('Koka','Kogut','1234')");
+//    $conn->exec("INSERT INTO users(name,surname,password)
+//      VALUES ('Koka','Kogut','1234')");
+//
+//    $conn->commit();
+//    echo "record created";
+//}
+//catch (PDOException $e) {
+//    $conn->rollBack();
+//}
